119b3de33184099648aed9024c6a2c0ee0b67e7f
Hishara Dilshan <hish2k15@gmail.com>
2022-07-08 00:40:34 +0530

    Added custom timeouts

--- app/src/main/java/com/hishd/retrofitcoroutines/apimanager/RetrofitInstance.kt
+++ app/src/main/java/com/hishd/retrofitcoroutines/apimanager/RetrofitInstance.kt
@@ -5,6 +5,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitInstance {
     private val BASE_URL = "https://jsonplaceholder.typicode.com/"
@@ -15,6 +16,12 @@ object RetrofitInstance {
 
     private val client = OkHttpClient.Builder().apply {
         this.addInterceptor(interceptor)
+        //Connect with server timeout
+        this.connectTimeout(30, TimeUnit.SECONDS)
+        //Response from server after connecting timeout
+        this.readTimeout(20, TimeUnit.SECONDS)
+        //Timeout for sending data packets to server
+        this.readTimeout(20, TimeUnit.SECONDS)
     }.build()
 
     fun getRetrofitInstance(): Retrofit {
